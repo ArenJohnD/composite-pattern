01cce8c6fbef3683afd8a73bb1e8d61da3d066b1
Aren John Esoen <152838648+ArenJohnD@users.noreply.github.com>
2025-05-13 12:53:59 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,7 +2,6 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Client {
-
     public static void main(String[] args) {
         College university = new College("New Era University");
 
